60a6c91961ec26493fcdc4d7cc1b07abd14d92f6
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-20 16:24:15 +0000

    minor fix

--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField, SubmitField, PasswordField, BooleanField, Selec
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import Players, Characters, Campaigns, Instances
 from flask_login import current_user
-from sqlalchemy import Query
 #--------------------------------------------------------------------------------------------
 #Character creation form
 class CharacterForm(FlaskForm):
