899faeea481277399c9cfccc650a709f1af9804a
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-20 15:27:11 +0000

    removed instances queries

--- application/routes.py
+++ application/routes.py
@@ -3,8 +3,6 @@ from application import app, db, bcrypt
 from application.models import Campaigns, Players, Characters, Instances
 from application.forms import CharacterForm, CampaignForm, RegistrationForm, LoginForm, UpdateAccountForm, InstanceForm
 from flask_login import login_user, current_user, logout_user, login_required
-from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField
-
 #---------------------------------------------------------------------------------------------------
 @app.route('/')
 @app.route('/home')
@@ -128,20 +126,19 @@ def account_delete():
 #---------------------------------------------------------------------------------------------------
 @app.route('/InstanceCreation', methods=['GET', 'POST'])#POSTS SUBSTITUTED FOR CHARACTERS, SECOND MODULE ROUTE FOR CAMPAIGN CREATION WITH SIMILARITY
 @login_required
-def instance(self, *args, **kwargs):
-    form = super(InstanceForm, self).__init__(*args, **kwargs)
-    form.campaign_id.choices = [(campaign.id) for campaign in Campaigns.query.all()]
-    form.character_id.choices = [(character.id) for character in Characters.query.all()]
-    return form
+def instance():
+    form = InstanceForm()
     if form.validate_on_submit():
         instanceData = Instances(
-        instance_name=form.instance_name.data,
-        location=form.location.data,
-        character_id = SelectField()
+            instance_name=form.instance_name.data,
+            location=form.location.data,   
         )
+
         db.session.add(instanceData)
         db.session.commit()
+
         return redirect(url_for('home'))
+
     else:
         print(form.errors)
 
