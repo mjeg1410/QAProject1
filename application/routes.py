be13e360d26b078946a86f6278f42b3e8609dee6
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-23 10:31:34 +0000

    another instances fix

--- application/routes.py
+++ application/routes.py
@@ -139,7 +139,7 @@ def instance():
         instanceData = Instances(
             Instance_name=form.instance_name.data,
             location=form.instance_location.data,
-            campaign_id=form.campaign_id.data
+            campaign_id=form.campaign_id.data,
             character_id=form.character_id.data
         )
         db.session.add(instanceData)
