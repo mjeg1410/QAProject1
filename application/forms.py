d6f7e53649309634ea2fb6c27c68f858968e0afb
mjeg1410 <michaeljohnedward@hotmail.com>
2020-03-23 10:21:26 +0000

    another instances fix

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, InputRequired
 from application.models import Players, Characters, Campaigns, Instances
 from flask_login import current_user
 #--------------------------------------------------------------------------------------------
